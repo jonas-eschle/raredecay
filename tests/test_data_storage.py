f40f50ad049fdf8fd9f60282ea4dc369c42fbd80
Jonas Eschle <mayou36@jonas.eschle.com>
2016-11-12 17:39:54 +0100

    - added unittest hello world (pytest)

---
+++ tests/test_data_storage.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python2
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Nov 12 17:29:38 2016
+
+@author: Jonas Eschle "Mayou36"
+"""
+
+def test_1():
+    assert 1 == 1
+
+def test_fail():
+    assert True is False
