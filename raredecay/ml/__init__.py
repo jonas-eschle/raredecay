4cdaf16750e7056aabd5b6ba30457059f4c3cc57
Jonas 'Mayou36' Eschle <mayou36@jonas.eschle.com>
2017-08-10 20:26:20 +0200

    test caching

--- raredecay/ml/__init__.py
+++ raredecay/ml/__init__.py
@@ -1,2 +1,4 @@
 from raredecay.analysis.ml_analysis import (classify, backward_feature_elimination, optimize_hyper_parameters,
                                             make_clf)
+from raredecay.analysis.physical_analysis import feature_exploration
+from raredecay.analysis.physical_analysis import final_training as sideband_training
