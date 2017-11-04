f34817bfa011112937a7a9f7d5ae14d7dd1b6ca6
Jonas 'Mayou36' Eschle <mayou36@jonas.eschle.com>
2017-11-04 19:45:51 +0100

    doc fix, travis update

--- raredecay/ml.py
+++ raredecay/ml.py
@@ -5,4 +5,4 @@ from raredecay.analysis.physical_analysis import feature_exploration
 from raredecay.analysis.physical_analysis import final_training as sideband_training
 
 __all__ = ['classify', 'backward_feature_elimination', 'optimize_hyper_parameters', 'make_clf',
-           'feature_exploration', 'sideband_trainingpi']
+           'feature_exploration', 'sideband_training']
