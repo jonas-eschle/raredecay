94b5378dfad7a26655a095cc474af2991f5527a3
Jonas Eschle 'Mayou36 <mayou36@jonas.eschle.com>
2018-11-19 18:14:38 +0100

    Use tarball for legacy rep

--- setup.py
+++ setup.py
@@ -67,7 +67,7 @@ if __name__ == '__main__':
           author='Jonas Eschle',
           author_email='mayou36@jonas.eschle.com',
           license='Apache-2.0 License',
-          #      dependency_links=['https://github.com/yandex/rep/archive/stratifiedkfold.zip'],
+          dependency_links=['https://github.com/REPlegacy/rep/tarball/master'],
           install_requires=requirements,
           extras_require=extras_require,
           packages=['raredecay',
